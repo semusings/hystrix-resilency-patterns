83a7ac7b30148b085ea2bfb7d30963898f63945b
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-05-19 09:14:55 +0545

    fix: hystrix

---
+++ src/main/java/io/github/bhuwanupadhyay/customer/Customer.java
@@ -0,0 +1,4 @@
+package io.github.bhuwanupadhyay.customer;
+
+public class Customer {
+}
---
+++ src/test/java/io/github/bhuwanupadhyay/customer/ExternalCustomerServiceTest.java
@@ -0,0 +1,7 @@
+package io.github.bhuwanupadhyay.customer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExternalCustomerServiceTest {
+
+}
